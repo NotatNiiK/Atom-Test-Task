23e82afbffb25d82b036add72b90d9ba6b53ee53
Anton Pogadaiko <pogadajko52@gmail.com>
2023-05-19 10:06:39 +0300

    final version

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+import LessonHeader from './components/LessonHeader.vue';
+import LessonMain from './components/LessonMain.vue';
+import lessons from './api/lessons'
 import { useStore } from './store';
 const store = useStore();
-console.log(store.count);
+console.log(store)
+const lessonsList = ref(lessons);
 
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <el-button type="primary" plain>Primary</el-button>
+  <div class="wrapper">
+    <LessonHeader />
+    <LessonMain :lessons-list="lessonsList"/>
+  </div>
 </template>
+
+<style>
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+[class*='__container']{
+  max-width: 1040px;
+  margin: 0 auto;
+}
+</style>
 
---
+++ src/components/LessonHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <div class="header__container py-3 flex justify-between items-center border-b-2 border-white border-solid">
+            <el-avatar :size="50" :src="circleUrl" class="cursor-pointer"/>
+            <nav class="header__nav">
+                <ul class="header__list flex items-center">
+                    <li class="header__item ml-6">
+                        <a href="#" class="header__link text-white text-lg transition-all hover:text-gray-600">Home</a>
+                    </li>
+                    <li class="header__item ml-6">
+                        <a href="#" class="header__link text-white text-lg transition-all hover:text-gray-600">About</a>
+                    </li>
+                    <li class="header__item ml-6">
+                        <a href="#" class="header__link text-white text-lg transition-all hover:text-gray-600">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+const circleUrl: string = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png';
+
+</script>
